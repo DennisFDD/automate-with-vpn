c052fbbdca04ba122eb3b859868515407eb90ab2
DennisFDD <93407709+DennisFDD@users.noreply.github.com>
2022-12-03 14:26:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@ data "ibm_pi_key" "ssh_key" {
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
 }
 
-
 data "ibm_pi_network" "network2" {
-  pi_network_name = "sn-cnx-vpn"
+  pi_network_name = "testvpn"
   pi_cloud_instance_id = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
 }
 
@@ -27,19 +26,5 @@ resource "ibm_pi_instance" "test-instance" {
 
     pi_network {
     network_id  = data.ibm_pi_network.network2.id
-  }
-
-      resource "ibm_pi_vpn_connection" "vpndfd" {
-        pi_cloud_instance_id    = "42175bd4-dc42-4ce0-ac6f-bc55caac4b7c"
-        pi_vpn_connection_name  = "vpndfd"
-        pi_ike_policy_id        = ibm_pi_ike_policy.policy.policy_id
-        pi_ipsec_policy_id      = ibm_pi_ipsec_policy.policy.policy_id
-        pi_vpn_connection_mode  = "policy"
-        pi_networks             = data.ibm_pi_network.network2.id
-        pi_peer_gateway_address = "10.240.0.4"
-        pi_peer_subnets         = ["10.24.0.0/24"]
-    }
-  
-  
-  
+  }  
 }
